6cea0098c3592b52f485b4db34e705baf77393d6
LN Renganarayana <lrengan2@gmail.com>
2017-02-10 17:59:58 -0800

    Printing Token info such as lemma and shape. Removing stop words.

--- ParsingText.py
+++ ParsingText.py
@@ -1,9 +1,13 @@
 from spacy.en import English
 import nltk
+import spacy
+
+#  to make spacy parser Ph.D as a single token
+#  spacy.en.English.Defaults.tokenizer_exceptions["Ph.D."] = [{"F": "Ph.D."}]
 
 parser = English()
 
-text = "I love Tacos. I have a Ph.D. in Tacology."
+text = "Paco loves Tacos. Paco has a Ph.D. in Tacology."
 tokens = parser(text)
 
 # print the sentences
@@ -12,6 +16,21 @@ for sent in tokens.sents:
     print(sent)
     print()
 
+#  token_orth gives the string representation of the token
+tokenList = [token.orth_ for token in tokens if not token.orth_.isspace()]
 
 
+def print_token(t):
+    print('value: ', t.orth_)
+    print('lemma: ', t.lemma_)
+    print('shape: ', t.shape_)
 
+for token in tokens:
+    print_token(token)
+
+english_stopwords = set(nltk.corpus.stopwords.words('english'))
+#  removing stop words
+
+tokenWithOutStopWords = [token.orth_ for token in tokens if token.orth_ not in english_stopwords]
+print("Tokens without stop words")
+print(tokenWithOutStopWords)
